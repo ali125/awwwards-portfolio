49618d085a2b6dcc5ac7afa26a1f3834a361cffc
Ali Mortazavi <ali.mortazavi121@gmail.com>
2025-08-14 18:54:15 +0330

    feat: complete hero section

---
+++ src/components/AnimatedTextLines.tsx
@@ -0,0 +1,51 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import type React from "react";
+import { useRef } from "react";
+
+gsap.registerPlugin(ScrollTrigger);
+
+type Props = {
+  text: string;
+  className?: string;
+};
+
+const AnimatedTextLines: React.FC<Props> = ({ text, className }) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const lineRefs = useRef<(HTMLElement | null)[]>([]);
+  const lines = text.split("\n").filter((line) => line.trim() !== "");
+
+  useGSAP(() => {
+    if (lineRefs.current.length > 0) {
+      gsap.from(lineRefs.current, {
+        y: 100,
+        opacity: 0,
+        duration: 1,
+        stagger: 0.3,
+        ease: "back.out",
+        scrollTrigger: {
+          trigger: containerRef.current,
+        },
+      });
+    }
+  });
+
+  return (
+    <div ref={containerRef} className={className}>
+      {lines.map((line, index) => (
+        <span
+          key={index}
+          ref={(el) => {
+            lineRefs.current[index] = el;
+          }}
+          className="block leading-relaxed tracking-wide text-pretty"
+        >
+          {line}
+        </span>
+      ))}
+    </div>
+  );
+};
+
+export default AnimatedTextLines;
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,8 +1,41 @@
 import { useRef } from "react";
+import AnimatedTextLines from "../AnimatedTextLines";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { Canvas } from "@react-three/fiber";
+import { PlanetModel } from "../Planet";
+import { Environment, Float, Lightformer } from "@react-three/drei";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
   const contextRef = useRef<HTMLDivElement>(null);
   const headerRef = useRef<HTMLDivElement>(null);
+
+  const isMobile = useMediaQuery({ maxWidth: 853 });
+
+  const aboutText = `I help growing brands and stratups gain an
+  \nunfair advantage through premium
+  \nresults driven webs/apps`;
+
+  useGSAP(() => {
+    const tl = gsap.timeline();
+    tl.from(contextRef.current, {
+      y: "50vh",
+      duration: 1,
+      ease: "circ.out",
+    });
+
+    tl.from(
+      headerRef.current,
+      {
+        opacity: 0,
+        y: 200,
+        duration: 1,
+        ease: "circ.out",
+      },
+      "<+0.2" // 0.2 seconds before the prev animation finished
+    );
+  }, []);
 
   return (
     <section id="home" className="flex flex-col justify-end min-h-screen">
@@ -25,13 +58,55 @@ const Hero = () => {
         <div className="relative px-10 text-black">
           <div className="absolute inset-x-0 border-t-2" />
           <div className="py-12 sm:py-16 text-end">
-            <p className="font-light uppercase value-text-responsive">
-              I help growing brands and stratups gain an unfair advantage
-              through premium results driven webs/apps
-            </p>
+            <AnimatedTextLines
+              text={aboutText}
+              className="font-light uppercase value-text-responsive"
+            />
           </div>
         </div>
       </div>
+      <figure
+        className="absolute inset-0 -z-50"
+        style={{ width: "100vw", height: "100vh" }}
+      >
+        <Canvas
+          shadows
+          camera={{ position: [0, 0, -10], fov: 17.5, near: 1, far: 20 }}
+        >
+          <ambientLight intensity={0.5} />
+          <Float speed={0.5}>
+            <PlanetModel scale={isMobile ? 0.6 : 1} />
+          </Float>
+          <Environment resolution={256}>
+            <group rotation={[-Math.PI / 3, 4, 1]}>
+              <Lightformer
+                form="circle"
+                intensity={2}
+                position={[0, 5, -9]}
+                scale={10}
+              />
+              <Lightformer
+                form="circle"
+                intensity={2}
+                position={[0, 3, 1]}
+                scale={10}
+              />
+              <Lightformer
+                form="circle"
+                intensity={2}
+                position={[-5, -1, -1]}
+                scale={10}
+              />
+              <Lightformer
+                form="circle"
+                intensity={2}
+                position={[10, 1, 0]}
+                scale={16}
+              />
+            </group>
+          </Environment>
+        </Canvas>
+      </figure>
     </section>
   );
 };
