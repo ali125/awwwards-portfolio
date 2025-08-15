28bb8d477c30f756bb5b2707835a674994c8e121
Ali Mortazavi <ali.mortazavi121@gmail.com>
2025-08-15 13:28:27 +0330

    feat: added service section

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
+import ReactLenis from "lenis/react";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Services from "./components/Services";
 import ServiceSummary from "./components/ServiceSummary";
 
 const App = () => {
   return (
-    <div className="relative w-screen min-h-screen overflow-x-auto">
+    <ReactLenis root className="relative w-screen min-h-screen overflow-x-auto">
       <Navbar />
       <Hero />
       <ServiceSummary />
+      <Services />
       <section className="min-h-screen" />
-    </div>
+    </ReactLenis>
   );
 };
 
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,70 +1,23 @@
-import { useRef } from "react";
-import AnimatedTextLines from "../AnimatedTextLines";
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
 import { Canvas } from "@react-three/fiber";
 import { PlanetModel } from "./Planet";
 import { Environment, Float, Lightformer } from "@react-three/drei";
 import { useMediaQuery } from "react-responsive";
+import AnimatedHeaderSection from "../AnimatedHeaderSection";
 
 const Hero = () => {
-  const contextRef = useRef<HTMLDivElement>(null);
-  const headerRef = useRef<HTMLDivElement>(null);
-
   const isMobile = useMediaQuery({ maxWidth: 853 });
 
-  const aboutText = `I help growing brands and stratups gain an
-  \nunfair advantage through premium
-  \nresults driven webs/apps`;
-
-  useGSAP(() => {
-    const tl = gsap.timeline();
-    tl.from(contextRef.current, {
-      y: "50vh",
-      duration: 1,
-      ease: "circ.out",
-    });
-
-    tl.from(
-      headerRef.current,
-      {
-        opacity: 0,
-        y: 200,
-        duration: 1,
-        ease: "circ.out",
-      },
-      "<+0.2" // 0.2 seconds before the prev animation finished
-    );
-  }, []);
+  const text = `I help growing brands and stratups gain an
+        \nunfair advantage through premium
+        \nresults driven webs/apps`;
 
   return (
     <section id="home" className="flex flex-col justify-end min-h-screen">
-      <div ref={contextRef}>
-        <div style={{ clipPath: "polygon(0 0, 100% 0%, 100% 100%, 0% 100%)" }}>
-          <div
-            ref={headerRef}
-            className="flex flex-col justify-center gap-12 pt-16 sm:gap-16"
-          >
-            <p className="text-sm font-light tracking-[0.5rem] uppercase px-10 text-black">
-              404 No Bugs Found
-            </p>
-            <div className="px-10">
-              <h1 className="flex flex-col flex-wrap gap-12 text-black uppercase banner-text-responsive sm:gap-16 md:block">
-                Ali Mortazavi
-              </h1>
-            </div>
-          </div>
-        </div>
-        <div className="relative px-10 text-black">
-          <div className="absolute inset-x-0 border-t-2" />
-          <div className="py-12 sm:py-16 text-end">
-            <AnimatedTextLines
-              text={aboutText}
-              className="font-light uppercase value-text-responsive"
-            />
-          </div>
-        </div>
-      </div>
+      <AnimatedHeaderSection
+        title="Ali Mortazavi"
+        subTitle="404 No Bugs Found"
+        text={text}
+      />
       <figure
         className="absolute inset-0 -z-50"
         style={{ width: "100vw", height: "100vh" }}
